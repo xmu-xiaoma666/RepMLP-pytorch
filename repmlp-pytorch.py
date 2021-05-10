c03f7fb5f5a63feb2ae121471443924271049016
xmu-xiaoma666 <775629340@qq.com>
2021-05-10 15:15:51 +0800

    annotation

--- repmlp-pytorch.py
+++ repmlp-pytorch.py
@@ -92,16 +92,16 @@ class RepMLP(nn.Module):
 
 
 if __name__ == '__main__':
-    N=4
-    C=512
-    O=1024
-    H=14
-    W=14
-    h=7
-    w=7
-    fc1_fc2_reduction=1
-    fc3_groups=8
-    repconv_kernels=[1,3,5,7]
+    N=4 #batch size
+    C=512 #input dim
+    O=1024 #output dim
+    H=14 #image height
+    W=14 #image width
+    h=7 #patch height
+    w=7 #patch width
+    fc1_fc2_reduction=1 #reduction ratio
+    fc3_groups=8 # groups
+    repconv_kernels=[1,3,5,7] #kernel list
     repmlp=RepMLP(C,H,W,h,w,fc1_fc2_reduction,fc3_groups,repconv_kernels=repconv_kernels)
     x=torch.randn(N,C,H,W)
     y=repmlp(x)
